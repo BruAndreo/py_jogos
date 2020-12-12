2370147463078ed9a16a2428f09b9edca5e86ed8
Bruno Andreo <brunolino2026@gmail.com>
2020-12-12 16:36:43 -0300

    Refactor em jogo de advinhacao

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,46 +1,41 @@
 import random
+import utils.util as utils
 
+numero_min = 1
+numero_max = 100
+pontuacao = 1000
 
-def jogar():
-
-    boas_vindas = "Olá seja bem-vindo ao jogo de adivinhação"
-    asteriscos = len(boas_vindas) * '*'
-
-    print(asteriscos)
-    print(boas_vindas)
-    print(asteriscos)
-
-    numero_min = 1
-    numero_max = 100
-    total_tentativas = 0
-    pontuacao = 1000
-    secret_number = random.randrange(numero_min, numero_max + 1)
-
-    print("Selecione o nível de dificuldade do jogo")
-    print("1 - Fácil (10 tentativas)")
-    print("2 - Médio (5 tentativas)")
-    print("3 - Dificil (3 tentativas)")
-
-    nivel = int(input("Nível de Dificuldade: "))
+def definir_numero_tentativas():
+    nivel = utils.selecionar_dificuldade()
 
     if nivel == 1:
-        total_tentativas = 10
+        return 10
     elif nivel == 2:
-        total_tentativas = 5
+        return 5
     elif nivel == 3: 
-        total_tentativas = 3
-    else:
-        print("É necessário informar um nível de dificuldade válido")
-        exit()
+        return 3
 
+def numero_fora_intervalo(num):
+    if ((num < numero_min) or (num > numero_max)):
+        print(f"Você deve digitar um numero entre {numero_min} e {numero_max}")
+        return True
+    return False
+
+def diminuir_pontuacao(pontos_perdidos):
+    pontuacao = pontuacao - pontos_perdidos
+
+def jogar():
+    utils.boas_vindas_msg("Bem vindo ao jogo de Adivinhação")
+
+    total_tentativas = definir_numero_tentativas()
+    secret_number = random.randrange(numero_min, numero_max + 1)
 
     for tentativa_atual in range(1, total_tentativas + 1):
-        print("Tentativa {} de {}".format(tentativa_atual, total_tentativas))
+        print("> Tentativa {}/{}".format(tentativa_atual, total_tentativas))
 
-        chute = int(input("Digite seu numero entre {} e {}: ".format(numero_min, numero_max)))
+        chute = int(input(">> Digite um numero entre {} e {}: ".format(numero_min, numero_max)))
 
-        if ((chute < numero_min) or (chute > numero_max)):
-            print(f"Você deve digitar um numero entre {numero_min} e {numero_max}")
+        if numero_fora_intervalo(chute):
             continue
 
         acertou = secret_number == chute
@@ -50,13 +45,13 @@ def jogar():
             break
         elif (secret_number < chute):
             print("Erroouu - É pra baixo")
-            pontuacao = pontuacao - (chute - secret_number)
+            diminuir_pontuacao(chute - secret_number)
         else:
             print("Erroouu - É pra cima")
-            pontuacao = pontuacao - (secret_number - chute)
+            diminuir_pontuacao(secret_number - chute)
 
     if not acertou:
-        pontuacao = 0
+        diminuir_pontuacao(pontuacao)
 
     print(f"Fim do jogo - O numero correto é: {secret_number}")
     print(f"Sua pontuação foi: {pontuacao}")
--- utils/util.py
+++ utils/util.py
@@ -5,3 +5,22 @@ def boas_vindas_msg(msg):
     print(asteriscos)
     print(msg)
     print(asteriscos)
+
+def selecionar_dificuldade():
+    opcao_invalida = True
+    while opcao_invalida:
+        print("Selecione o nível de dificuldade do jogo")
+        print("1 - Fácil")
+        print("2 - Médio")
+        print("3 - Dificil")
+        print("0 - Desistir do Jogo")
+
+        nivel = int(input("Nível de Dificuldade: "))
+
+        if nivel in [1,2,3]:
+            return nivel
+        elif nivel == 0:
+            print("Até logo")
+            exit()
+        else:
+            print("É necessário informar um nível de dificuldade válido")
